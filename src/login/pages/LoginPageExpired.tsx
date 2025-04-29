825cf355879415581377fc0f33dafc0e504e67bc
flx <92300914+felixhoebel@users.noreply.github.com>
2025-04-29 17:06:00 +0200

    login expired page

---
+++ src/login/pages/LoginPageExpired.tsx
@@ -0,0 +1,30 @@
+import type { PageProps } from "keycloakify/login/pages/PageProps";
+import type { KcContext } from "../KcContext";
+import type { I18n } from "../i18n";
+import { buttonVariants } from "../../components/ui/button";
+
+
+export default function LoginPageExpired(props: PageProps<Extract<KcContext, { pageId: "login-page-expired.ftl" }>, I18n>) {
+    const { kcContext, i18n, doUseDefaultCss, Template, classes } = props;
+
+    const { url } = kcContext;
+
+    const { msg } = i18n;
+
+    return (
+        <Template kcContext={kcContext} i18n={i18n} doUseDefaultCss={doUseDefaultCss} classes={classes} headerNode={msg("pageExpiredTitle")}>
+            <div id="instruction1" className="flex flex-col gap-4 text-sm">
+                {msg("pageExpiredMsg1")}
+                <a id="loginRestartLink" href={url.loginRestartFlowUrl} className={buttonVariants({ variant: "outline" })}>
+                    {msg("doClickHere")}
+                </a>
+                <div className="w-full h-[1px] my-4 bg-muted/50" />
+                {msg("pageExpiredMsg2")}{" "}
+                <a id="loginContinueLink" href={url.loginAction} className={buttonVariants({ variant: "outline" })}>
+                    {msg("doClickHere")}
+                </a>{" "}
+
+            </div>
+        </Template>
+    );
+}
